95924d6ce4a2c6fcb6b67541ccb62aa119682580
Sudhanshu Sharma <shudhanshu0511max@gmail.com>
2021-12-22 20:49:47 +0530

    corrected styling

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,25 +1,32 @@
 const AddTask = () => {
   return (
-    <div className="mx">
+    <div class="mx">
       <form>
-        <div className="my-1">
-          <label className="form-label">Task</label>
-          <input type="text" className="form-control" placeholder="Add Task" />
+        <div class="my-1">
+          <label class="form-label">Task</label>
+          <input type="text" id="task-detail" class="form-control" required />
         </div>
-        <div className="my-1">
-          <label className="form-label">Date and Time</label>
+        <div class="my-1">
+          <label class="form-label">Date and Time</label>
           <input
-            className="form-control"
             type="datetime-local"
-            placeholder="Add Date and Time"
+            id="date-time-detail"
+            class="form-control"
+            required
           />
         </div>
-
-        <div className="form-switch my-1">
-          <label className="form-check-label">Set Reminder</label>
-          <input className="form-check-input form-control" type="checkbox" />
+        <div class="my-1 pt-1 mydiv">
+          <div class="ms-3 form-switch form-check">
+            <label class="form-check-label">Set Reminder</label>
+            <input
+              class="form-check-input form-check-control"
+              type="checkbox"
+            />
+          </div>
+          <button type="button" id="save" class="btn btn-primary">
+            Save Task
+          </button>
         </div>
-        <input type="submit" value="Save Task" className="btn btn-primary" />
       </form>
     </div>
   );
